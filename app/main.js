b5a3d5d5fbccb77572eea3c122f8e31aa731173f
chenkechao <chenkechao123@163.com>
2017-08-20 01:07:56 +0800

    componentt

--- app/main.js
+++ app/main.js
@@ -4,8 +4,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Component1 from './components/Component1.jsx';
+import TopAppBar from './components/TopAppBar.jsx';
+import BasicTable from './components/Tables.jsx';
+
+var Content = React.createClass({
+    render:function render() {
+        return <div><TopAppBar/><BasicTable/></div>;
+    }
+});
 
 ReactDom.render(
-    <Component1 />,
+    <Content />,
     document.getElementById('content')
 );
